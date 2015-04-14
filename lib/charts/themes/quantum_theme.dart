7e93b49d15497b336f1cab3c5e4df523374889a5
Prasad Sunkari <s.prasad@gmail.com>
2015-04-14 10:50:06 -0700

    Reduce deviation on blur for drop-shadow

--- lib/charts/themes/quantum_theme.dart
+++ lib/charts/themes/quantum_theme.dart
@@ -65,7 +65,7 @@ class QuantumChartTheme extends ChartTheme {
       <feOffset result="offOut" in="SourceGraphic" dx="0" dy="0" />
       <feColorMatrix result="matrixOut" in="offOut"
           type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"/>
-      <feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="3" />
+      <feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="2" />
       <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
     </filter>
 ''';
