c25ace868e0831bb121dff270b664f27a8d10ce4
Sam Rawlins <srawlins@google.com>
2018-01-29 16:28:51 -0800

    Different fix

--- lib/charts/src/cartesian_area_impl.dart
+++ lib/charts/src/cartesian_area_impl.dart
@@ -423,8 +423,8 @@ class DefaultCartesianAreaImpl implements CartesianArea {
     config.dimensions.take(dimensionAxesCount).forEach((int column) {
       var axis = _getDimensionAxis(column),
           sampleColumnSpec = data.columns.elementAt(column),
-          values = data.rows.map((row) => row.elementAt(column))
-              as Iterable<Comparable>;
+          values = data.rows
+              .map<Comparable>((row) => row.elementAt(column) as Comparable);
       List domain;
 
       if (sampleColumnSpec.useOrdinalScale) {
